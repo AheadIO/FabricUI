77a1a8ad14a0c3f9a1f9e3697dafdd492f9bc376
Helge Mathee <helge.mathee@fabricengine.com>
2015-04-15 18:27:24 +0200

    FE-4106: Qt color scheme

--- Style/FabricStyle.h
+++ Style/FabricStyle.h
@@ -19,38 +19,6 @@ namespace FabricUI
         FabricStyle() {}
 
         void polish(QPalette &palette);
-    //     void polish(QWidget *widget);
-    //     void unpolish(QWidget *widget);
-    //     int styleHint(StyleHint which, const QStyleOption *option,
-    //                   const QWidget *widget = 0,
-    //                   QStyleHintReturn *returnData = 0) const;
-    //     int pixelMetric(PixelMetric which, const QStyleOption *option,
-    //                     const QWidget *widget = 0) const;
-    //     void drawPrimitive(PrimitiveElement which,
-    //                        const QStyleOption *option, QPainter *painter,
-    //                        const QWidget *widget = 0) const;
-    //     void drawComplexControl(ComplexControl which,
-    //                             const QStyleOptionComplex *option,
-    //                             QPainter *painter,
-    //                             const QWidget *widget = 0) const;
-    //     QRect subControlRect(ComplexControl whichControl,
-    //                          const QStyleOptionComplex *option,
-    //                          SubControl whichSubControl,
-    //                          const QWidget *widget = 0) const;
-    // public slots:
-    //     QIcon standardIconImplementation(StandardPixmap which,
-    //                                      const QStyleOption *option,
-    //                                      const QWidget *widget = 0) const;
-    // private:
-    //     void drawFabricFrame(const QStyleOption *option,
-    //                          QPainter *painter) const;
-    //     void drawFabricBevel(const QStyleOption *option,
-    //                          QPainter *painter) const;
-    //     void drawFabricCheckBoxIndicator(const QStyleOption *option,
-    //                                      QPainter *painter) const;
-    //     void drawFabricSpinBoxButton(SubControl which,
-    //                                  const QStyleOptionComplex *option,
-    //                                  QPainter *painter) const;
     };
 
   };
