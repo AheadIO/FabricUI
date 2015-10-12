812bcdbd4e28c079eb2731eb86bb37eb4304c655
Helge Mathee <helge.mathee@fabricengine.com>
2015-10-12 12:23:08 +0200

    scenehubstandalone: centralizing viewport related widgets below FabricUI - reusing of QtToKlEvent

---
+++ Viewports/RTRGLContext.h
@@ -0,0 +1,71 @@
+#ifndef _H_RTRGLContext_
+#define _H_RTRGLContext_
+
+#include <QtOpenGL/QGLWidget>
+
+#ifdef __APPLE__
+void *TryFormat(
+  bool doubleBuffer,
+  bool stereo,
+  bool alpha,
+  int alphaBufferSize,
+  bool stencil,
+  int stencilBufferSize,
+  bool depth,
+  int depthBufferSize,
+  bool accum,
+  int accumBufferSize,
+  bool sampleBuffers,
+  int samples
+  );
+
+inline void *TryFormat( QGLFormat const &qglFormat )
+{
+  return TryFormat(
+    qglFormat.doubleBuffer(),
+    qglFormat.stereo(),
+    qglFormat.alpha(),
+    qglFormat.alphaBufferSize(),
+    qglFormat.stencil(),
+    qglFormat.stencilBufferSize(),
+    qglFormat.depth(),
+    qglFormat.depthBufferSize(),
+    qglFormat.accum(),
+    qglFormat.accumBufferSize(),
+    qglFormat.sampleBuffers(),
+    qglFormat.samples()
+    );
+}
+#endif
+
+class RTRGLContext : public QGLContext
+{
+  QGLFormat m_qglFormat;
+
+public:
+
+  RTRGLContext( QGLFormat const &qglFormat )
+    : QGLContext( qglFormat )
+  {
+  }
+
+#ifdef __APPLE__
+  virtual void *chooseMacVisual(GDHandle /* handle */)
+  {
+    void *fmt = TryFormat(m_qglFormat);
+    if (!fmt && m_qglFormat.stereo()) {
+        m_qglFormat.setStereo(false);
+        fmt = TryFormat(m_qglFormat);
+    }
+    if (!fmt && m_qglFormat.sampleBuffers()) {
+        m_qglFormat.setSampleBuffers(false);
+        fmt = TryFormat(m_qglFormat);
+    }
+    if (!fmt)
+        qWarning("QGLContext::chooseMacVisual: Unable to choose a pixel format");
+    return fmt;
+  }
+#endif
+};
+
+#endif // _H_RTRGLContext_
---
+++ Viewports/RTRGLViewportWidget.h
@@ -0,0 +1,89 @@
+// Copyright 2010-2015 Fabric Software Inc. All rights reserved.
+
+#ifndef __FABRICUI_VIEWPORTS_RTRGLVIEWPORTWIDGET__
+#define __FABRICUI_VIEWPORTS_RTRGLVIEWPORTWIDGET__
+
+// #include <GL/glew.h>
+
+#include <QtCore/QtCore>
+#include <QtGui/QMouseEvent>
+#include <QtGui/QDrag>
+#include <QtGui/QTextEdit>
+#include <QtGui/QMainWindow>
+#include <QtGui/QImage>
+#include <QtGui/QDialog>
+#include <QtOpenGL/QGLWidget>
+#include <QtCore/QTime>
+#include <QtGui/QMenuBar>
+#include <FabricCore.h>
+#include <FabricUI/SceneHub/SGLightManagerDialog.h>
+#include <FabricUI/SceneHub/SGGeometryManagerDialog.h>
+
+class RTRGLViewportWidget : public QGLWidget
+{
+  Q_OBJECT
+
+  public:
+    RTRGLViewportWidget(FabricCore::Client *, FabricCore::RTVal, QGLContext *, QWidget *parent = NULL);
+    virtual ~RTRGLViewportWidget() {};
+
+    void initialize();
+    void resetRTVals();
+    void setTime(float time);
+    void toggleAlwaysRefresh();
+    bool alwaysRefreshes() { return m_alwaysRefresh; }
+    double fps() const { return m_fps; }
+
+  signals:
+    void onDrop();
+    void manipsAcceptedEvent();
+
+  public slots:
+    void onContextMenu(const QPoint &point);
+    void editObjectColor();
+    void editLocalObjectColor();
+
+    void editLightProperties();
+    void addArchive();
+    void addLight();
+    void addTexture();
+
+  private:
+    void constuctAddMenu();
+    void constuctLightMenu();
+    void constuctGeometryMenu(std::string category);
+    void addExternalFile(QStringList, QPoint, bool);
+    void editObjectColor( bool local );
+  
+  protected:
+    virtual void paintGL();
+    virtual void resizeGL(int w, int h);
+
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void wheelEvent(QWheelEvent *event);
+    bool onMouseEvent(QEvent *event);
+    void keyPressEvent(QKeyEvent *event);
+    void dragEnterEvent(QDragEnterEvent *event);
+    void dragMoveEvent(QDragMoveEvent *event);
+    void dropEvent(QDropEvent *event);
+
+    QWidget *m_parent;
+    FabricCore::Client *m_client;
+    FabricCore::RTVal m_viewport;
+    FabricCore::RTVal m_testObject;
+    FabricCore::RTVal m_width;
+    FabricCore::RTVal m_height;
+
+    QPoint m_screenPos;
+    QTime m_fpsTimer;
+    double m_fps;
+    double m_fpsStack[16];
+    bool m_alwaysRefresh;
+
+    FabricUI::SceneHub::SGBaseManagerDialog *m_geometryDialog;
+    FabricUI::SceneHub::SGLightManagerDialog *m_lightDialog;
+};
+
+#endif // __FABRICUI_VIEWPORTS_RTRGLVIEWPORTWIDGET__
