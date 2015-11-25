463e4041b9e57f4be3c13a032b58291cf5f22c5b
Eric Mootz <eric@mootzoid.com>
2015-11-25 12:09:41 +0100

    FE-5695 Fabric for Maya now listening to 'varInserted' and 'varRemoved'

--- DFG/DFGCombinedWidget.cpp
+++ DFG/DFGCombinedWidget.cpp
@@ -149,7 +149,7 @@ void DFGCombinedWidget::init(
 
       QObject::connect(m_dfgWidget, SIGNAL(onGraphSet(FabricUI::GraphView::Graph*)), 
         this, SLOT(onGraphSet(FabricUI::GraphView::Graph*)));
-      QObject::connect(m_dfgWidget->getUIController(), SIGNAL(varsChanged()), m_treeWidget, SLOT(refresh()));
+
       QObject::connect(m_dfgWidget, SIGNAL(newPresetSaved(QString)), m_treeWidget, SLOT(refresh()));
     }
 
