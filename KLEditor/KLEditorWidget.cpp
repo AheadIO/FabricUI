f9a1e021df53dfc7a20f55e452016997ebedb9b3
Eric Mootz <eric@mootzoid.com>
2016-01-06 13:40:14 +0100

    FE-4184 set the default text color and background color of the KL source code widget

--- KLEditor/KLEditorWidget.cpp
+++ KLEditor/KLEditorWidget.cpp
@@ -17,10 +17,6 @@ KLEditorWidget::KLEditorWidget(QWidget * parent, FabricServices::ASTWrapper::KLA
   m_lineNumbers = new LineNumberWidget(this, config);
   m_sourceCodeWidget = new KLSourceCodeWidget(this, manager, config);
 
-QPalette p = m_sourceCodeWidget->palette();
-p.setColor(QPalette::Base, QColor(255,0,0));
-m_sourceCodeWidget->setPalette(p);
-
   QObject::connect(m_sourceCodeWidget, SIGNAL(documentScrolled(unsigned int)), m_lineNumbers, SLOT(setLineOffset(unsigned int)));
 
   QHBoxLayout * layout = new QHBoxLayout();
