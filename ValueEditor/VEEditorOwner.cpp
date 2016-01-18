3e32f7a3d7b5392eba74e213cd337e5f245decdc
Stephen Taylor <stephen.taylor.dev@gmail.com>
2016-01-18 10:51:50 -0500

    Fixed regression which caused exception on frame change

--- ValueEditor/VEEditorOwner.cpp
+++ ValueEditor/VEEditorOwner.cpp
@@ -466,30 +466,30 @@ void VEEditorOwner::onFrameChanged( int frame )
     FabricCore::DFGBinding binding =
       getUIController()->getBinding();
     FabricCore::DFGExec exec = binding.getExec();
-    FabricCore::Client client; // = binding.getClient();
+    FabricCore::Context ctxt = binding.getHost().getContext();
 
     if (exec.isExecPortResolvedType( m_timelinePortIndex, "SInt32" ))
       binding.setArgValue(
         m_timelinePortIndex,
-        FabricCore::RTVal::ConstructSInt32( client, frame ),
+        FabricCore::RTVal::ConstructSInt32( ctxt, frame ),
         false
         );
     else if (exec.isExecPortResolvedType( m_timelinePortIndex, "UInt32" ))
       binding.setArgValue(
         m_timelinePortIndex,
-        FabricCore::RTVal::ConstructUInt32( client, frame ),
+        FabricCore::RTVal::ConstructUInt32( ctxt, frame ),
         false
         );
     else if (exec.isExecPortResolvedType( m_timelinePortIndex, "Float32" ))
       binding.setArgValue(
         m_timelinePortIndex,
-        FabricCore::RTVal::ConstructFloat32( client, frame ),
+        FabricCore::RTVal::ConstructFloat32( ctxt, frame ),
         false
         );
     else if (exec.isExecPortResolvedType( m_timelinePortIndex, "Float64" ))
       binding.setArgValue(
         m_timelinePortIndex,
-        FabricCore::RTVal::ConstructFloat64( client, frame ),
+        FabricCore::RTVal::ConstructFloat64( ctxt, frame ),
         false
         );
   }
