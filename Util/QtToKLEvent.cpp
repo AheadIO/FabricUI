bb896b7854c0b1ec1fe425ca15a236d387a8c6aa
Julien Couet <couet.julien@gmail.com>
2016-01-28 12:11:27 -0500

    switch tool from menu

--- Util/QtToKLEvent.cpp
+++ Util/QtToKLEvent.cpp
@@ -26,80 +26,65 @@ FabricCore::RTVal QtToKLEvent(QEvent *event, FabricCore::Client const& client, F
 
   if(event->type() == QEvent::Enter)
   {
-    // FABRIC_TRY_RETURN("ManipulationTool::onEvent", false,
-      klevent = FabricCore::RTVal::Create(client, "MouseEvent", 0, 0);
-    // );
+    klevent = FabricCore::RTVal::Create(client, "MouseEvent", 0, 0);
   }
+  
   else if(event->type() == QEvent::Leave)
   {
-    // FABRIC_TRY_RETURN("ManipulationTool::onEvent", false,
-      klevent = FabricCore::RTVal::Create(client, "MouseEvent", 0, 0);
-    // );
+    klevent = FabricCore::RTVal::Create(client, "MouseEvent", 0, 0);
   }
+
   else if (event->type() == QEvent::KeyPress || event->type() == QEvent::KeyRelease) 
   {
     QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
-
-    // FABRIC_TRY_RETURN("ManipulationTool::onEvent", false,
-      klevent = FabricCore::RTVal::Create(client, "KeyEvent", 0, 0);
-      klevent.setMember("key", FabricCore::RTVal::ConstructUInt32(client, keyEvent->key()));
-      klevent.setMember("count", FabricCore::RTVal::ConstructUInt32(client, keyEvent->count()));
-      klevent.setMember("isAutoRepeat", FabricCore::RTVal::ConstructBoolean(client, keyEvent->isAutoRepeat()));
-    // );
+    klevent = FabricCore::RTVal::Create(client, "KeyEvent", 0, 0);
+    klevent.setMember("key", FabricCore::RTVal::ConstructUInt32(client, keyEvent->key()));
+    klevent.setMember("count", FabricCore::RTVal::ConstructUInt32(client, keyEvent->count()));
+    klevent.setMember("isAutoRepeat", FabricCore::RTVal::ConstructBoolean(client, keyEvent->isAutoRepeat()));
   } 
-  else if ( event->type() == QEvent::MouseMove || 
-            event->type() == QEvent::MouseButtonDblClick || 
-            event->type() == QEvent::MouseButtonPress || 
-            event->type() == QEvent::MouseButtonRelease
-          ) 
+
+  else if(event->type() == QEvent::MouseMove || 
+          event->type() == QEvent::MouseButtonDblClick || 
+          event->type() == QEvent::MouseButtonPress || 
+          event->type() == QEvent::MouseButtonRelease) 
   {
     QMouseEvent *mouseEvent = static_cast<QMouseEvent *>(event);
-
-    // FABRIC_TRY_RETURN("ManipulationTool::onEvent", false,
-      klevent = FabricCore::RTVal::Create(client, "MouseEvent", 0, 0);
-
-      FabricCore::RTVal klpos = QtToKLMousePosition(mouseEvent->pos(), client, viewport, swapAxis);
-
-      klevent.setMember("button", FabricCore::RTVal::ConstructUInt32(client, mouseEvent->button()));
-      klevent.setMember("buttons", FabricCore::RTVal::ConstructUInt32(client, mouseEvent->buttons()));
-      klevent.setMember("pos", klpos);
-    // );
+    klevent = FabricCore::RTVal::Create(client, "MouseEvent", 0, 0);
+    FabricCore::RTVal klpos = QtToKLMousePosition(mouseEvent->pos(), client, viewport, swapAxis);
+    klevent.setMember("button", FabricCore::RTVal::ConstructUInt32(client, mouseEvent->button()));
+    klevent.setMember("buttons", FabricCore::RTVal::ConstructUInt32(client, mouseEvent->buttons()));
+    klevent.setMember("pos", klpos);
   } 
+
   else if (event->type() == QEvent::Wheel) 
   {
     QWheelEvent *mouseWheelEvent = static_cast<QWheelEvent *>(event);
-
-    // FABRIC_TRY_RETURN("ManipulationTool::onEvent", false,
-      klevent = FabricCore::RTVal::Create(client, "MouseWheelEvent", 0, 0);
-
-      FabricCore::RTVal klpos = QtToKLMousePosition(mouseWheelEvent->pos(), client, viewport, swapAxis);
-
-      klevent.setMember("buttons", FabricCore::RTVal::ConstructUInt32(client, mouseWheelEvent->buttons()));
-      klevent.setMember("delta", FabricCore::RTVal::ConstructSInt32(client, mouseWheelEvent->delta()));
-      klevent.setMember("pos", klpos);
-    // );
+    klevent = FabricCore::RTVal::Create(client, "MouseWheelEvent", 0, 0);
+    FabricCore::RTVal klpos = QtToKLMousePosition(mouseWheelEvent->pos(), client, viewport, swapAxis);
+    klevent.setMember("buttons", FabricCore::RTVal::ConstructUInt32(client, mouseWheelEvent->buttons()));
+    klevent.setMember("delta", FabricCore::RTVal::ConstructSInt32(client, mouseWheelEvent->delta()));
+    klevent.setMember("pos", klpos);
   }
 
   if(klevent.isValid())
   {
     int eventType = int(event->type());
-
     // FABRIC_TRY_RETURN("ManipulationTool::onEvent", false,
-      klevent.setMember("eventType", FabricCore::RTVal::ConstructUInt32(client, eventType));
+    klevent.setMember("eventType", FabricCore::RTVal::ConstructUInt32(client, eventType));
 
-      QInputEvent *inputEvent = static_cast<QInputEvent *>(event);
-      klevent.setMember("modifiers", FabricCore::RTVal::ConstructUInt32(client, inputEvent->modifiers()));
+    QInputEvent *inputEvent = static_cast<QInputEvent *>(event);
+    klevent.setMember("modifiers", FabricCore::RTVal::ConstructUInt32(client, inputEvent->modifiers()));
 
-      //////////////////////////
-      // Setup the viewport
-      klevent.setMember("viewport", viewport);
+    //////////////////////////
+    // Setup the viewport
+    klevent.setMember("viewport", viewport);
 
-      //////////////////////////
-      // Setup the Host
-      // We cannot set an interface value via RTVals.
-      FabricCore::RTVal host = FabricCore::RTVal::Create(client, "Host", 0, 0);
-      host.setMember("hostName", FabricCore::RTVal::ConstructString(client, "Canvas"));
-      klevent.setMember("host", host);
+    //////////////////////////
+    // Setup the Host
+    // We cannot set an interface value via RTVals.
+    FabricCore::RTVal host = FabricCore::RTVal::Create(client, "Host", 0, 0);
+    host.setMember("hostName", FabricCore::RTVal::ConstructString(client, "Canvas"));
+    klevent.setMember("host", host);
   }
   return klevent;
 }
