8900a05b195544325dd6f759ffcbdfdf0cf56924
Peter Zion <peter.zion@gmail.com>
2016-02-04 15:27:38 -0500

    FE-6028: ensure that set.value is not editable when it is connected

--- ModelItems/SetPortItemMetadata.h
+++ ModelItems/SetPortItemMetadata.h
@@ -13,16 +13,25 @@ namespace ModelItems {
 
 class SetPortItemMetadata : public NodePortItemMetadata
 {
+protected:
+
+  SetPortModelItem *m_setPortModelItem;
+
 public:
 
   SetPortItemMetadata( SetPortModelItem *setPortModelItem )
-    : NodePortItemMetadata( setPortModelItem ) {}
+    : NodePortItemMetadata( setPortModelItem )
+    , m_setPortModelItem( setPortModelItem )
+    {}
 
   virtual const char* getString( const char* key ) const /*override*/
   {
     if ( key == VENotInspectableKey )
     {
-      return FTL_STR("").c_str();
+      FabricCore::DFGExec exec = m_setPortModelItem->getExec();
+      FTL::CStrRef portPath = m_setPortModelItem->getPortPath();
+      bool isNotInspectable = exec.hasSrcPort( portPath.c_str() );
+      return isNotInspectable? FTL_STR("1").c_str(): FTL_STR("").c_str();
     }
 
     return NodePortItemMetadata::getString( key );
