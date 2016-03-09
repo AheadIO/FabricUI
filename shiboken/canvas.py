de8d7e1393a18c286f8b452a8a5f231e512d6054
Andrew MacPherson <andrew.macp@gmail.com>
2016-03-09 14:10:43 +0100

    FE-6285: Use explicit client.close() in canvas.py

--- shiboken/canvas.py
+++ shiboken/canvas.py
@@ -416,6 +416,9 @@ class MainWindow(DFG.DFGMainWindow):
 
         QtGui.QMainWindow.closeEvent(self, event)
 
+        self.valueEditor = None
+        self.client.close()
+
         if os.path.exists(self.autosaveFilename):
             os.remove(self.autosaveFilename)
 
