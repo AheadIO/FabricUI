a703f0c3bb95524c417813c33efe5c1b4725e27e
Eric <Eric>
2016-03-31 10:41:34 +0200

    FE-6344 remove TABs in FilepathViewItem.h

--- ValueEditor/FilepathViewItem.h
+++ ValueEditor/FilepathViewItem.h
@@ -26,16 +26,16 @@ public:
     );
   static const int Priority;
 
-	FilepathViewItem(
+  FilepathViewItem(
     QString const &name,
     QVariant const &value,
     ItemMetadata* metadata
     );
-	~FilepathViewItem();
+  ~FilepathViewItem();
 
-	QWidget *getWidget() /*override*/;
+  QWidget *getWidget() /*override*/;
   
-	void onModelValueChanged( QVariant const &value ) /*override*/;
+  void onModelValueChanged( QVariant const &value ) /*override*/;
 
   void deleteMe() /*override*/ { delete this; }
 
@@ -44,12 +44,12 @@ public:
 
 private:
 
-	VELineEdit* m_edit;
+  VELineEdit* m_edit;
   QWidget *m_widget;
   QString m_filter;
 
 private slots:
 
-	void onTextModified( QString text );
+  void onTextModified( QString text );
   void doBrowse();
 };
